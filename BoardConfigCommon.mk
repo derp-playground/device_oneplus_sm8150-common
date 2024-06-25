53727fcc1a2b102cf0dc1b788b5f244f529a514e
Matheus <mattvieira123@gmail.com>
2024-06-25 10:19:59 -0300

    BCM: inherit QTI and Derpfest FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -204,4 +204,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # FCM
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(VENDOR_PATH)/configs/vintf/oneplus_vendor_framework_compatibility_matrix.xml
+    $(VENDOR_PATH)/configs/vintf/oneplus_vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
+    vendor/derp/config/device_framework_matrix.xml
