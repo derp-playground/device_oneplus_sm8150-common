692985979aea6f559e90e869064593813215ca07
Matheus <mattvieira123@gmail.com>
2024-10-21 10:38:15 -0300

    common: build UDFPS icons package

--- common.mk
+++ common.mk
@@ -449,6 +449,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
 
+# UDFPS icons
+EXTRA_UDFPS_ICONS := true
+
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
